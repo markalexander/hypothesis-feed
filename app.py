2978274b0a67ba15043d2a863f833299eaf94d52
Mark Alexander <git@markalexander.se>
2018-06-23 15:50:44 +0100

    Switch to separate config file

--- app.py
+++ app.py
@@ -6,23 +6,22 @@ from datetime import datetime
 from pyatom import AtomFeed
 from flask import Flask, request, Response
 from flask_caching import Cache
+from config import Config as cfg
 
-token = '6879-be75929bd9dda036fd326ce112ecf65f'  # https://hypothes.is/account/developer
-group = 'LLaJXJ4w'  # E.g. from the group stream URI
 
 app = Flask(__name__)
-cache = Cache(app, config={'CACHE_TYPE': 'simple'})
+cache = Cache(app, config={'CACHE_TYPE': cfg.cache_type})
 
 
 @app.route('/feed.atom')
-@cache.cached(timeout=10*60)
+@cache.cached(timeout=cfg.cache_timeout)
 def atom():
     feed = AtomFeed(title='Machine Learning and Pattern Recognition - Hypothes.is Feed',
                     feed_url=request.url,
-                    url='https://hypothes.is/stream?q=group:%s' % group,
+                    url='https://hypothes.is/stream?q=group:%s' % cfg.group,
                     author='Iain Murray')
-    r = requests.get('https://hypothes.is/api/search?group=%s' % group,
-                     headers={'Authorization': 'Bearer %s' % token})
+    r = requests.get('https://hypothes.is/api/search?group=%s' % cfg.group,
+                     headers={'Authorization': 'Bearer %s' % cfg.token})
     for row in json.loads(r.text)['rows']:
         feed.add(title=row['document']['title'][0], content=row['text'],
                  content_type='text', author=row['user'], url=row['links']['html'],
---
+++ config.example.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+class Config:
+
+    token = '6879-be75929bd9dda036fd326ce112ecf65f'  # https://hypothes.is/account/developer
+    group = 'LLaJXJ4w'  # E.g. from the group stream URI
+    cache_type = 'simple'
+    cache_timeout = 600  # In seconds
