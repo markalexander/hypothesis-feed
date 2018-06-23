01e9374f2ebe4494205a66ee61c4abcfedcbcbb3
Mark Alexander <git@markalexander.se>
2018-06-23 16:40:36 +0100

    PEP8 fixes

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from datetime import datetime
 from pyatom import AtomFeed
 from flask import Flask, request, Response, redirect, url_for
 from flask_caching import Cache
-from config import Config as cfg
+from config import Config as Config
 
 
 app = Flask(__name__)
-cache = Cache(app, config={'CACHE_TYPE': cfg.CACHE_TYPE})
+cache = Cache(app, config={'CACHE_TYPE': Config.CACHE_TYPE})
 
 
 @app.route('/')
@@ -19,19 +19,27 @@ def index():
 
 
 @app.route('/feed.atom')
-@cache.cached(timeout=cfg.CACHE_TIMEOUT)
+@cache.cached(timeout=Config.CACHE_TIMEOUT)
 def atom():
-    feed = AtomFeed(title=cfg.FEED_TITLE,
+    feed = AtomFeed(title=Config.FEED_TITLE,
                     feed_url=request.url,
-                    url='https://hypothes.is/stream?q=group:%s' % cfg.HYPOTHESIS_GROUP_ID,
-                    author=cfg.FEED_AUTHOR)
-    r = requests.get('https://hypothes.is/api/search?group=%s' % cfg.HYPOTHESIS_GROUP_ID,
-                     headers={'Authorization': 'Bearer %s' % cfg.HYPOTHESIS_API_TOKEN})
+                    url='https://hypothes.is/stream?q=group:%s'
+                        % Config.HYPOTHESIS_GROUP_ID,
+                    author=Config.FEED_AUTHOR)
+    r = requests.get('https://hypothes.is/api/search?group=%s'
+                     % Config.HYPOTHESIS_GROUP_ID,
+                     headers={'Authorization': 'Bearer %s'
+                                               % Config.HYPOTHESIS_API_TOKEN})
     for post in json.loads(r.text)['rows']:
-        feed.add(title=post['document']['title'][0], content=post['text'],
-                 content_type='text', author=post['user'], url=post['links']['html'],
-                 updated=datetime.strptime(post['updated'][:-13], '%Y-%m-%dT%H:%M:%S'),
-                 published=datetime.strptime(post['created'][:-13], '%Y-%m-%dT%H:%M:%S'))
+        feed.add(title=post['document']['title'][0],
+                 content=post['text'],
+                 content_type='text',
+                 author=post['user'],
+                 url=post['links']['html'],
+                 updated=datetime.strptime(post['updated'][:-13],
+                                           '%Y-%m-%dT%H:%M:%S'),
+                 published=datetime.strptime(post['created'][:-13],
+                                             '%Y-%m-%dT%H:%M:%S'))
     return Response(feed.to_string(), mimetype='application/atom+xml')
 
 
