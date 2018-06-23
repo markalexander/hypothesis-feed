c6d721e4b418fae1aac848a9ace47872ce740f6d
Mark Alexander <git@markalexander.se>
2018-06-23 16:38:10 +0100

    Add comments to config file example

--- config.example.py
+++ config.example.py
@@ -3,9 +3,27 @@
 
 class Config:
 
+    # The title of the feed
     FEED_TITLE = 'Some Group Name Here - Hypothes.is Feed'
-    FEED_AUTHOR = 'Joe Bloggs'
-    HYPOTHESIS_API_TOKEN = ''  # https://hypothes.is/account/developer
-    HYPOTHESIS_GROUP_ID = 'LLaJXJ4w'  # E.g. from the group stream URI
+
+    # The author of the feed
+    FEED_AUTHOR = 'Various'
+
+    # Your hypothes.is API token.
+    # Visit https://hypothes.is/account/developer when signed in to generate
+    # one
+    HYPOTHESIS_API_TOKEN = ''
+
+    # The ID of the group to generate the feed from.
+    # You can grab this from various places, e.g. the URL for the group stream.
+    HYPOTHESIS_GROUP_ID = ''
+
+    # The type of cache to use in flask-caching
+    # I suggest to start with 'simple', which saves to local disk and should
+    # work in most cases.  If you have trouble with that, you can turn of the
+    # cache by setting this to 'null'.  See the flask-caching docs for more
+    # advanced usage
     CACHE_TYPE = 'simple'
-    CACHE_TIMEOUT = 600  # In seconds
+
+    # How long (in seconds) to serve up a cached API response before flushing
+    CACHE_TIMEOUT = 600
